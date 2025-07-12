67b0c177cf88ab3bba5cca53785471c2f781eeee
CariP03 <pasqualecaringellab@gmail.com>
2025-07-12 23:51:39 +0200

    feat: add function to create a TP-Link plug object
    Add a function that uses the library "PyP100" to create a TP-Link plug
    object.

---
+++ src/plug_init.py
@@ -0,0 +1,14 @@
+from PyP100 import PyP100
+from dotenv import load_dotenv
+import os
+
+from ip_finder import find_ip_by_mac
+
+def plug_init():
+    load_dotenv() # load variables
+
+    # find plug ip address
+    plug_ip = find_ip_by_mac(os.getenv('PLUG_MAC'))
+
+    # create plug object
+    return PyP100.P100(plug_ip, os.getenv("EMAIL"), os.getenv("PASSWORD"))
