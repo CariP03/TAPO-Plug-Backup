b7db4c97ab26283ffa18ac6e728e9dc0a4670e2a
CariP03 <pasqualecaringellab@gmail.com>
2025-07-12 23:32:33 +0200

    feat: add function to find IP by MAC
    Add a function that uses the library "scapy" to send an ARP packet to
    find the IP address by the MAC address.

---
+++ src/ip_finder.py
@@ -0,0 +1,18 @@
+from scapy.layers.l2 import ARP, Ether
+from scapy.sendrecv import srp
+import time
+
+
+def find_ip_by_mac(target_mac, subnet="192.168.1.0/24", attempts=4, timeout=4):
+    arp = ARP(pdst=subnet)
+    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+    packet = ether / arp
+
+    for i in range(attempts):
+        result = srp(packet, timeout=timeout, verbose=False)[0]
+        for sent, received in result:
+            if received.hwsrc.lower() == target_mac.lower():
+                return received.psrc
+        time.sleep(1)
+
+    return None
